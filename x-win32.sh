741977cf5f4efe4dedac8a103f64936ce676a40f
Robin Gareus <robin@gareus.org>
2013-02-23 15:55:10 +0100

    exit if x-win failes

--- x-win32.sh
+++ x-win32.sh
@@ -23,23 +23,23 @@ mkdir -p $NSIDIR
 echo $NSIDIR
 
 WINEBIN=${WINEROOT}/bin/
-cp -v src/harvid $NSIDIR/harvid.exe
-i686-w64-mingw32-strip $NSIDIR/harvid.exe
+cp -v src/harvid $NSIDIR/harvid.exe || exit
+i686-w64-mingw32-strip $NSIDIR/harvid.exe || exit
 
 dllssrc="avformat avcodec avdevice avutil swscale swresample avfilter avresample postproc jpeg"
 for fname in $dllssrc; do
-	cp -v `readlink -f ${WINEROOT}/bin/lib${fname}.dll` $NSIDIR
+	cp -v `readlink -f ${WINEROOT}/bin/lib${fname}.dll` $NSIDIR || exit
 done
 
-cp -v $WINEBIN/zlib1.dll $NSIDIR
-cp -v $WINEBIN/pthreadGC2.dll $NSIDIR
-cp -v $WINEBIN/cygwin1.dll $NSIDIR
+cp -v $WINEBIN/zlib1.dll $NSIDIR || exit
+cp -v $WINEBIN/pthreadGC2.dll $NSIDIR || exit
+cp -v $WINEBIN/cygwin1.dll $NSIDIR || exit
 
 sed 's/@VERSION@/'${VERSION}'/' pkg/win/harvid.nsi > $NSIDIR/harvid.nsi
-wine "$NSISEXE" "Z:/$NSIDIR/harvid.nsi"
+wine "$NSISEXE" "Z:/$NSIDIR/harvid.nsi" || exit
 
 echo "--- DONE ---"
-ls -l $NSIDIR/*exe
+ls -l $NSIDIR/*exe || exit
 
 test -d site/releases/ || exit
 cp $NSIDIR/harvid_installer-*exe site/releases/
